dcdcf83a602825fc77cf4f598c40ee231f7665df
MarceloRavache <jm.ravache@outlook.com>
2021-07-29 14:59:23 -0300

    update: configs updated

--- src/main/java/com/rede_social/rede_social/RedeSocialApplication.java
+++ src/main/java/com/rede_social/rede_social/RedeSocialApplication.java
@@ -2,8 +2,10 @@ package com.rede_social.rede_social;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 @SpringBootApplication
+@EnableReactiveMongoRepositories
 public class RedeSocialApplication {
 
 	public static void main(String[] args) {
