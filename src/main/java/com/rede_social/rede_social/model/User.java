0b71b0929dbdf1fdf9759d9e04d5c081a25ff1f6
MarceloRavache <jm.ravache@outlook.com>
2021-07-29 15:00:00 -0300

    update: created model for User

---
+++ src/main/java/com/rede_social/rede_social/model/User.java
@@ -0,0 +1,55 @@
+package com.rede_social.rede_social.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Document(collation = "users")
+public class User {
+
+    @Id
+    private String id;
+
+    @NotBlank
+    @Size(max = 255)
+    private String email;
+
+    @NotBlank
+    @Size(max = 255)
+    private String password;
+
+    public User(String id, String email, String password) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
