8875ab2d706a0f42a86a4e83de320e4ff05bd489
MarceloRavache <jm.ravache@outlook.com>
2021-07-31 18:39:14 -0300

    update: update Model User

--- src/main/java/com/rede_social/rede_social/model/User.java
+++ src/main/java/com/rede_social/rede_social/model/User.java
@@ -7,7 +7,7 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 import java.util.Objects;
 
-@Document(collation = "users")
+@Document
 public class User {
 
     @Id
