63ba670afcbe021b8dedb965997a278ff211ff33
MarceloRavache <jm.ravache@outlook.com>
2021-07-31 18:39:40 -0300

    update: update Model Comment

--- src/main/java/com/rede_social/rede_social/model/Comment.java
+++ src/main/java/com/rede_social/rede_social/model/Comment.java
@@ -7,7 +7,7 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 import java.util.Objects;
 
-@Document(collation = "comments")
+@Document
 public class Comment {
     @Id
     private String id;
